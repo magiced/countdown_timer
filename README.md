ccefc0da12718915d6ee3ad8f76903a3527acd9e
Bisdee <ed.bisdee@enerpac.com>
2023-03-27 09:41:36 +0100

    update from work comp

--- README.md
+++ README.md
@@ -15,12 +15,31 @@ install using pip: `pip install simpleaudio`
 + tick.wav <https://commons.wikimedia.org/wiki/File:Clock_ticking.ogg> - edited using Audacity
 + brown_noise.wav - created using Audacity's generate noise function
 
+## References
+
+* https://tkdocs.com/shipman/
+* https://tkdocs.com/tutorial/morewidgets.html#progressbar
+* https://www.pythontutorial.net/tkinter/tkinter-progressbar/
+
 ## TODO
 
-+ make the brown noise wav smaller
-+ change all tk widgets to ttk to make it prettier
-+ change clock font to a digital clock font
-+ add json files for the settings so that the noises and default times can be changed
-+ add menus for tick noise and end signal options
-+ add volume controls for each noise
-+ change colours of buttons etc to visually fit together better
+* [ ] convert to OOP
+    - to enable making more timers easily
+* [ ] make the brown noise wav smaller
+* [ ] make it prettier
+    * [ ] make all colours coherent
+    * [ ] change all tk widgets to ttk to make it prettier
+* [ ] change clock font to a digital clock font
+* [ ] add json files for the settings so that the noises and default times can be changed
+* [ ] add menus for tick noise and end signal options
+* [ ] add volume controls for each noise
+* [ ] change colours of buttons etc to visually fit together better
+    * [ ] can i use ttk for this?
+* [x] add progress bar
+    * https://tkdocs.com/tutorial/morewidgets.html#progressbar
+* [ ] can progress bar be put in title, or similar?
+* [ ] add count up mode, a la the dr seuss timer
+* [x] make jump to front of screen when timer ends
+
+## Count Up Timer
+When press start and time is 0, start counting upwards until 99:99:99
--- countdown_timer.py
+++ countdown_timer.py
@@ -37,7 +37,9 @@ def toggle_timer():
     if b_timer_running is False:
         # target_time = time.time() + delay_time + 1
         # target_time = time.time() + scl_time.get() + 1
-        target_time = time.time() + float(cbo_time.get())*60 + 1
+        delay_time = float(cbo_time.get())*60
+        print(delay_time)
+        target_time = time.time() + delay_time + 1
         b_timer_running = True
         btn_start["text"] = "STOP"
         if b_noise.get():
@@ -56,8 +58,13 @@ def reset_timer():
     b_timer_running = False
     target_time = 0
 
+def jump_to_front():
+    root.attributes('-topmost', True)
+    root.attributes('-topmost', False)
+
 def update_display():
     global b_timer_running
+    global delay_time
     global target_time
     global time_string_last
     global tick_wave_obj
@@ -65,15 +72,21 @@ def update_display():
 
     if b_timer_running:
         time_left = target_time - time.time()
+        time_percentage = (time_left/delay_time) * 100
+        # calc percentage time left here
         time_string = time.strftime("%H:%M:%S",time.gmtime(time_left))
         if (target_time > time.time()):
             if time_string != time_string_last:
                 clock['text'] = time_string
                 root.title(time_string)
+                # update progress bar here
+                progressbar['value'] = time_percentage
                 if b_tick.get():
                     tick_play_obj = tick_wave_obj.play()
         else:
             toggle_timer()
+            # progressbar is zero
+            jump_to_front()
             if b_end_signal.get():
                 alarm_play_obj = alarm_wave_obj.play()
         time_string_last = time_string
@@ -86,23 +99,31 @@ def update_display():
 """ GUI """
 root.title("TIMER")
 clock = tk.Label(root, font=('times', 40, 'bold'), fg='green',bg='black')
-scl_time = tk.Scale(root, from_=0, to_=60, orient="horizontal")
-cbo_time = ttk.Combobox(root,values=(0.1,1,2,3,4,5,10,15,20,25,30))
-btn_start = ttk.Button(root, text='START', command=toggle_timer)
+# scl_time = tk.Scale(root, from_=0, to_=60, orient="horizontal")
+progressbar = ttk.Progressbar(root, orient='horizontal', length=100, mode='determinate')
+
+# start button and time frame
+frame_start = tk.Frame(root)
+cbo_time = ttk.Combobox(frame_start,values=(0.01,0.1,1,2,3,4,5,10,15,20,25,30))
+btn_start = ttk.Button(frame_start, text='START', command=toggle_timer)
+cbo_time.grid(row=0, column=0, )
+cbo_time.current(8)
+btn_start.grid(row=0, column=1, columnspan=2, sticky="EW")
+
+# checkbox frame
 frame_chkbx = tk.Frame(root)
 chk_tick = tk.Checkbutton(frame_chkbx, text="TICK", variable=b_tick)
 chk_noise = tk.Checkbutton(frame_chkbx, text="NOISE", variable=b_noise)
 chk_end_signal = tk.Checkbutton(frame_chkbx, text="ALARM", variable=b_end_signal)
+chk_noise.grid(     row=0, column=0,    sticky="E")
+chk_tick.grid(      row=0, column=1,    sticky="EW")
+chk_end_signal.grid(row=0, column=2,    sticky="W")
 
-clock.grid(row=0, column=0, columnspan=3, sticky="EW")
-cbo_time.grid(row=1, column=0, )
-cbo_time.current(8)
-btn_start.grid(row=1, column=1, columnspan=2, sticky="EW")
-
-frame_chkbx.grid(row=3, column=0, columnspan=3)
-chk_noise.grid(row=0, column=0)
-chk_tick.grid(row=0, column=1)
-chk_end_signal.grid(row=0, column=2)
+# Main window layout
+clock.grid(         row=0,  column=0, columnspan=3, sticky="EW")
+progressbar.grid(   row=1,  column=0, columnspan=3, sticky="EW")
+frame_start.grid(   row=2,  column=0, columnspan=3, sticky="EW")
+frame_chkbx.grid(   row=3,  column=0, columnspan=3, sticky="EW")
 
 update_display()  # first call so that after x time the display is updated
 root.mainloop()
